9d72eeb92cdf10cd4f1db745a596a65b3b3093b9
Joe Spall <jspall98@aol.com>
2020-04-13 17:02:38 -0400

    Added comments, clarified variable names

--- code/crosswalk_detection.py
+++ code/crosswalk_detection.py
@@ -17,6 +17,7 @@ from itertools import groupby
 
 
 ######### CONSTANTS #########
+
 # Sampling STFT properties
 n_fft = 1024
 hop_length = int(float(n_fft)/4)
@@ -32,16 +33,19 @@ beep_period_time = 1.0133333333333334
 beep_period_time_variance = 0.02666666666666667
 
 # (in hertz)
+# Range of frequencies to keep (2500 to 2800 Hz for crosswalk audio beeps)
 harmonic_freq_ranges = [[-1,-1],
                         [2500,2800]]
 # (in dB)
+### Make value more negative to be more lenient on labeling (more False Positives).
+### Make value more positive to be more strict on labeling (more True Negatives).
 minimum_freq_threshold_db = -40
 
 
 
 def foreground_isolation(audio, sample_rate = 480000):
     """
-    Isolate foreground audio
+    Isolate foreground audio.
 
     Used to isolate foreground audio. Based on https://librosa.github.io/librosa_gallery/
     auto_examples/plot_vocal_separation.html. Looks at the STFT of the audio input, generates a
@@ -57,6 +61,7 @@ def foreground_isolation(audio, sample_rate = 480000):
     S_foreground (numpy.ndarray): Spectagram of the foreground waveform.
     """
 
+    # Evaluates STFT of entered audio
     print("Foreground isolation: 0%", end='\r')
     S_full, phase = librosa.magphase(librosa.stft(audio,n_fft=n_fft))
     print("Foreground isolation: 10%", end='\r')
@@ -67,14 +72,16 @@ def foreground_isolation(audio, sample_rate = 480000):
     # separated by at least 2 seconds.
     #
     # This suppresses sparse/non-repetetitive deviations from the average spectrum,
-    # and works well to discard vocal elements.
+    # and works well to discard the crosswalk beeps.
+
+    # *NOTE* -THIS IS THE MOST TIME CONSUMING STEP OF THE PROCESS. IT MAY APPEAR TO HANG HERE
     S_filter = librosa.decompose.nn_filter(S_full,
                                            aggregate=np.median,
                                            metric='cosine',
                                            width=int(librosa.time_to_frames(2, sr=sample_rate)))
     print("Foreground isolation: 80%", end='\r')
     # The output of the filter shouldn't be greater than the input
-    # if we assume signals are additive.  Taking the pointwise minimium
+    # if we assume signals are additive. Taking the pointwise minimium
     # with the input spectrum forces this.
     S_filter = np.minimum(S_full, S_filter)
     print("Foreground isolation: 90%", end='\r')
@@ -89,7 +96,7 @@ def foreground_isolation(audio, sample_rate = 480000):
                                    power=power)
 
     # Once we have the masks, simply multiply them with the input spectrum
-    # to separate thharmonic_freq_rangese components
+    # to separate the harmonic_freq_range components
 
     S_foreground = mask_v * S_full
     print("Foreground isolation: 95%", end='\r')
@@ -99,8 +106,8 @@ def foreground_isolation(audio, sample_rate = 480000):
 
     return isolated_audio, S_full, S_foreground
 
-def crosswalk_isolation(audio,frequencies,sample_rate = 480000,*,beep_duration= 3,beep_period = 188, beep_period_variance=3,
-                        amplitude_threshold=-999):
+def crosswalk_isolation(audio,frequencies,sample_rate = 480000,*,beep_duration= 3,beep_period = 188,
+                        beep_period_variance=3, amplitude_threshold=-999):
     """
     Isolate crosswalk beeping
 
@@ -127,14 +134,14 @@ def crosswalk_isolation(audio,frequencies,sample_rate = 480000,*,beep_duration= 
 
     """
     print("Crosswalk isolation: 0%", end='\r')
-    # Begin isolation
+    # Begin isolation by looking at STFT of audio.
     STFT_results = np.abs(librosa.stft(audio,n_fft=n_fft))
 
     print("Crosswalk isolation: 10%", end='\r')
-    # Calculates frequency bins
+    # Calculates frequency bins.
     fft_freqs = librosa.fft_frequencies(sr=sample_rate, n_fft=n_fft)
 
-    # Collect STFT index of focus frequency ranges
+    # Collect STFT index of focus frequency ranges.
     freq_indexes = np.array([])
     for harmonic in frequencies:
         current_indexes = np.nonzero((fft_freqs>=harmonic[0]) & (fft_freqs<=harmonic[1]))[0]
@@ -142,15 +149,16 @@ def crosswalk_isolation(audio,frequencies,sample_rate = 480000,*,beep_duration= 
 
     freq_indexes = freq_indexes.astype(int)
 
-    # Frequency of actual bins in ranges
+    # Frequency of actual bins in ranges.
     real_freqs = fft_freqs[freq_indexes]
 
-    #Sets all components not in crosswalk band frequency to zero
+    # Sets all components not in crosswalk band frequency to zero.
     data = np.zeros(STFT_results.shape)
     data[freq_indexes,:] = STFT_results[freq_indexes,:]
 
     print("Crosswalk isolation: 20%", end='\r')
-    # Performs thresholding on amplitude if described
+    # Performs thresholding on amplitude if described. Adjust "minimum_freq_threshold_db" in constants to make
+    # evaluation more or less aggressive.
     if amplitude_threshold != -999:
         data[data < amplitude_threshold] = 0
 
@@ -159,38 +167,58 @@ def crosswalk_isolation(audio,frequencies,sample_rate = 480000,*,beep_duration= 
     freq_rows = freq_indexes.size
     found_crosswalk = False
 
+    # Applies isolation per frequency bin row
     for row_ind in range(freq_rows):
+        # Finds indexes in frame (effectively time) where there is nonzero amplitude.
         non_zero_row = np.nonzero(data[freq_indexes[row_ind],:])[0]
+        # Used to only keep valid duration and period pulses, zeros out remaining values.
         new_row = np.zeros(data_frame_length)
-        # TODO Toggle duration and period
-        if non_zero_row.size > 0:
-            valid_pos = []
 
+        if non_zero_row.size > 0:
+
+            pulse_duration_valid = []
+
+            ### Duration of pulses ###
+            # Only allows sound pulses that are a minimum length.
+
+            # Checks for consecutive STFT frames that amplitude is non-zero per frequency bin.
             for k,g in groupby(enumerate(non_zero_row),lambda x:x[0]-x[1]):
                 group = (map(itemgetter(1),g))
                 group = list(map(int,group))
+                # If the current cluster has a long enough duration, it will be added to the list of valid.
                 if len(group) >= beep_duration:
-                    valid_pos.append((group[0],group[-1]))
+                    pulse_duration_valid.append((group[0],group[-1]))
 
-            new_valid = np.array([])
+            period_valid = np.array([])
 
-            #dependent on a beat, needs beep either before or after
-            if len(valid_pos) > 1:
+            ### Period of pulses ###
+            # Only allows consecutive pulses.
+
+            # Checks if any beeps with the minimum length are found.
+            if len(pulse_duration_valid) > 1:
+                # Goes through all minimum length pulses and determines if they are near other pulses (effectively
+                # clustering).
                 prev_mean = 0
-                for ele_ind in range(0,len(valid_pos)-1,1):
-                    curr_mean = int((valid_pos[ele_ind][0] + valid_pos[ele_ind][1])/2)
-                    future_mean = int((valid_pos[ele_ind+1][0] + valid_pos[ele_ind+1][1])/2)
+                for ele_ind in range(0,len(pulse_duration_valid)-1,1):
+                    # Checks before and after current pulse to determine string of pulses.
+                    # Averages position of pulse in frames (effectively time) and compares the position one pulse
+                    # before and after.
+                    curr_mean = int((pulse_duration_valid[ele_ind][0] + pulse_duration_valid[ele_ind][1])/2)
+                    future_mean = int((pulse_duration_valid[ele_ind+1][0] + pulse_duration_valid[ele_ind+1][1])/2)
                     diff_1 = curr_mean-prev_mean
                     diff_2 = future_mean-curr_mean
                     prev_mean = curr_mean
                     if((beep_period-beep_period_variance < diff_1 < beep_period+beep_period_variance)
                        or (beep_period-beep_period_variance < diff_2 < beep_period+beep_period_variance)):
-                        new_valid = np.hstack((new_valid,np.arange(valid_pos[ele_ind][0],valid_pos[ele_ind][1])))
+                        # If current pulse position is within range of other pulses, keep
+                        period_valid = np.hstack((period_valid,np.arange(pulse_duration_valid[ele_ind][0],
+                                                                         pulse_duration_valid[ele_ind][1])))
 
-            new_valid = np.unique(new_valid).astype(int)
+            period_valid = np.unique(period_valid).astype(int)
 
-            if len(new_valid) > 1:
-                new_row[new_valid] = data[freq_indexes[row_ind],new_valid]
+            if len(period_valid) > 1:
+                # Only keeps STFT magnitude amplitude values, zeros out remainder
+                new_row[period_valid] = data[freq_indexes[row_ind],period_valid]
                 found_crosswalk = True
 
         data[freq_indexes[row_ind],:] = new_row
@@ -214,22 +242,38 @@ def crosswalk_times(audio,sample_rate = 480000):
     times ([float]): The list of times in seconds during which a crosswalk started.
 
     """
+
+    # Determines the start of a beat in frames
     onset_frames = librosa.onset.onset_detect(y=audio, sr=sample_rate)
+    # Converts frames to seconds
     times = librosa.frames_to_time(onset_frames, sr=sample_rate)
     return times
 
 def crosswalk_audio_label(audio_file_name):
+    """
+    Labels crosswalk audio sounds for a given sound file.
 
+    Parameters:
+    audio_file_name (str): The audio file name for which crosswalk audio will be searched.
+
+    Returns:
+    found_crosswalk (bool): True if found crosswalk audio in the current file. False if not.
+
+    """
+    found_crosswalk = False
     audio_name = audio_file_name.split(".")[0]
 
+    # Uses sample rate of the original audio
     y , sample_rate = librosa.load(audio_file_name,sr=None)
+
+    #Verifies audio is valid
     if not librosa.util.valid_audio(y):
         print(audio_file_name + " not valid audio!")
         return
     else:
         print("Found " + audio_file_name)
 
-
+    # Isolates the "foreground" of the audio. Removes background noise and serves as first filtering step
     foreground_isolated_audio, S_full, S_foreground = foreground_isolation(y,sample_rate)
 
 
@@ -238,9 +282,12 @@ def crosswalk_audio_label(audio_file_name):
     beep_period_frames = librosa.time_to_frames(beep_period_time,sr=sample_rate, hop_length=hop_length)-2
     beep_period_frames_variance = librosa.time_to_frames(beep_period_time_variance,sr=sample_rate,
                                                          hop_length=hop_length)-2
-
     minimum_freq_threshold_amp = librosa.db_to_amplitude(minimum_freq_threshold_db)
 
+    # Looks for parts of audio that follow patterns scene from analysis:
+    # 1. Beeps are at a consistent frequency
+    # 2. Beeps have a consistent duration
+    # 3. Beeps have a consistent period of repetition
     local_STFT_results,found_crosswalk = crosswalk_isolation(foreground_isolated_audio,
                                              frequencies=harmonic_freq_ranges,
                                              sample_rate = sample_rate,
@@ -252,11 +299,13 @@ def crosswalk_audio_label(audio_file_name):
     filtered_isolation_audio = librosa.istft(local_STFT_results)
 
     if found_crosswalk:
+        # If crosswalk audio is found, the occurance of the beeps are tagged
         times = crosswalk_times(filtered_isolation_audio,sample_rate)
         if len(times) > 0:
             access_type = ""
             text_file_path = audio_name + ".txt"
-            #TODO consider text writing methods
+            # If a text file with the same name exists, the new labels will be appended. Otherwise, a new file is
+            # made
             if os.path.isfile(text_file_path):
                 access_type = "a"
                 print(text_file_path + " exists, will append.")
@@ -265,6 +314,7 @@ def crosswalk_audio_label(audio_file_name):
                 print(text_file_path + " does not exist, will make new file.")
 
             output_text_file = open(text_file_path, access_type)
+            # Cycles through all discovered start times and adds the beep_duration_time for the end times
             for time_element in times:
                 start_time = int(time_element*1000)
                 end_time = int(start_time + beep_duration_time*1000)
@@ -272,24 +322,26 @@ def crosswalk_audio_label(audio_file_name):
 
             output_text_file.close()
             print("Crosswalk audio found in " + audio_file_name + " and documented in " + text_file_path)
-            return True
+            found_crosswalk = True
     else:
         print("No crosswalk audio found in " + audio_file_name)
-        return False
+
+    return found_crosswalk
 
 def main():
     print("Starting crosswalk label...")
+    #Checks if enough input arguments are present
     if len(sys.argv) > 1:
         directory_option = sys.argv[1]
-        #changes directory if necessary
-        if directory_option == "-c":
-            print("Running on current directory.")
-        elif directory_option == "-d":
+        #changes directory if "-d", stays with current directory if "-c"
+
+        if directory_option == "-d":
             new_directory = sys.argv[2]
             os.chdir(new_directory)
             print("Running on " + new_directory + ".")
-
-        #changes extracts files and runs on wavs
+        else:
+            print("Running on current directory.")
+        #checks directory for files and runs on wavs
         current_path = os.getcwd()
         onlyfiles = [f for f in listdir(current_path) if isfile(join(current_path, f))]
         audio_files = 0
